bdd25990a44a7ba8ed16b192035338060fae7530
Christian <Christiannelby@hotmail.com>
2018-12-10 15:34:05 +0100

    ModelBuilder

---
+++ BackEnd/Models/Category_ComponentType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd.Models
+{
+    public partial class Category_ComponentType
+    {
+        public int CategoryId { get; set; }
+        public int ComponentTypeId { get; set; }
+    }
+}
