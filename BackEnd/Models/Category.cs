81aa611239a1adb8b5d068637b0d878d30150969
Christian <Christiannelby@hotmail.com>
2018-12-10 14:17:17 +0100

    Models

---
+++ BackEnd/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd.Models
+{
+    public partial class Category
+    {
+        //Taget direkte fra opgavebeskrivelsen
+
+        //public Category()
+        //{
+        //    ComponentTypes = new List<ComponentType>();
+        //}
+        //public int CategoryId { get; set; }
+        //public string Name { get; set; }
+        //public ICollection<ComponentType> ComponentTypes { get; protected set; }
+
+
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
