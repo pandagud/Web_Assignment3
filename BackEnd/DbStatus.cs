b9ad342dd8585e32e4000b9aaef1d5a9d0062efe
pandagud <pandagud@gmail.com>
2018-12-10 14:17:48 +0100

    LoginIsWorking

---
+++ BackEnd/DbStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BackEnd.Models;
+
+namespace BackEnd
+{
+    public class DbStatus
+    {
+        public bool success { get; set; }
+
+        public string errormessage { get; set; }
+
+        public Admin adminuser { get; set; }
+    }
+}
