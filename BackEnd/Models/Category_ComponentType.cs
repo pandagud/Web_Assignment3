53b3ea6a11ae5b3c6047dbe3e70fbcb3a2a48ac7
Christian <Christiannelby@hotmail.com>
2018-12-10 15:44:52 +0100

    Category_ComponentType pimary tilføjet

--- BackEnd/Models/Category_ComponentType.cs
+++ BackEnd/Models/Category_ComponentType.cs
@@ -6,6 +6,8 @@ namespace BackEnd.Models
 {
     public partial class Category_ComponentType
     {
+
+        public int Category_ComponentTypeId { get; set; }
         public int CategoryId { get; set; }
         public int ComponentTypeId { get; set; }
     }
--- WebAssignment3/Models/Category/Category.cs
+++ WebAssignment3/Models/Category/Category.cs
@@ -16,5 +16,8 @@ namespace WebAssignment3.Models.Category
         public int CategoryId { get; set; }
         public string Name { get; set; }
         public ICollection<ComponentType> ComponentTypes { get; protected set; }
+
+
+
     }
 }
